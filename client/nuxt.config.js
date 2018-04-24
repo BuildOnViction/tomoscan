ec9dea74a679313e9ca41ce4eb58fa6562c8775c
dinhln89 <lenhatdinh@gmail.com>
2018-04-24 10:22:42 +0700

    Fixed minor bug project title and remove redundant icon asset css file.

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -9,17 +9,13 @@ module.exports = {
       {charset: 'utf-8'}, {
         name: 'viewport', content: 'width=device-width, initial-scale=1',
       }, {
-        hid: 'description', name: 'description', content: 'Nuxt.js project',
+        hid: 'description', name: 'description', content: 'TOMO Explorer project',
       },
     ], link: [
       {
         rel: 'icon',
         type: 'image/x-icon',
         href: '/favicon.ico',
-      },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons',
       },
       {
         rel: 'stylesheet',
