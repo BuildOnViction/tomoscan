1fc3e074a03d4e255ff745fcc7772430cf8013de
dinhln89 <lenhatdinh@gmail.com>
2018-05-03 14:04:33 +0700

    Fixed search input for tx, address and block.

--- client/components/Search.vue
+++ client/components/Search.vue
@@ -1,6 +1,6 @@
 <template>
 	<b-navbar-nav class="ml-auto">
-		<b-form-input v-model="search" size="sm" class="mr-sm-2" type="text" placeholder="Search Address / TX / Block" v-on:keyup.enter="onGotoRoute"/>
+		<input type="search" v-model="search" class="form-control form-control-sm" placeholder="Search Address / TX / Block" @keyup.enter="onGotoRoute"/>
 		<b-button variant="primary" size="sm" class="my-2 my-sm-0" @click="onGotoRoute"><i class="fa fa-search"></i></b-button>
 	</b-navbar-nav>
 </template>
@@ -12,23 +12,21 @@
     }),
     methods: {
       onGotoRoute () {
-        let search = this.search
+        let search = this.search.trim()
         let regexpTx = /[0-9a-zA-Z]{66}?/
         let regexpAddr = /^(0x)?[0-9a-f]{40}$/
         let regexpBlock = /[0-9]+?/
         let to = null
 
-        if (regexpTx.test(search)) {
-          to = {name: 'txs-slug', params: {slug: search}}
-        }
-        else if (regexpAddr.test(search)) {
+        if (regexpAddr.test(search)) {
           to = {name: 'address-slug', params: {slug: search}}
+        }
+        else if (regexpTx.test(search)) {
+          to = {name: 'txs-slug', params: {slug: search}}
         }
         else if (regexpBlock.test(search)) {
           to = {name: 'blocks-slug', params: {slug: search}}
         }
-
-        console.log(to)
 
         if (!to) {
           return false
