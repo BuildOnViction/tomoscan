d7d64b37aa607f1c916695e48eda992fe9e05077
dinhln89 <lenhatdinh@gmail.com>
2018-05-10 16:59:15 +0700

    Fixed multibyte character problem when using web3js.

--- server/src/helpers/utils.js
+++ server/src/helpers/utils.js
@@ -39,10 +39,10 @@ export const paginate = async (
   }
 }
 
-export const trimWord = (word) => word.replace(/^\s+|\s+$|\s+(?=\s)/g, '').
-  replace('\t', '').
-  replace(/[^\x00-\x7F]/g, '').
-  trim()
+export const trimWord = (word) => utf8Encode(
+  word.replace(/^\s+|\s+$|\s+(?=\s)/g, '').
+    replace('\t', '').
+    trim())
 
 export const getSigner = (block) => {
   let signer = null
@@ -87,4 +87,66 @@ export const unformatAddress = (address) => address.replace(
 
 export const formatAddress = (address) => address.replace('0x',
   '0x000000000000000000000000')
+
+/**
+ * Encodes multi-byte Unicode string into utf-8 multiple single-byte characters
+ * (BMP / basic multilingual plane only).
+ *
+ * Chars in range U+0080 - U+07FF are encoded in 2 chars, U+0800 - U+FFFF in 3 chars.
+ *
+ * Can be achieved in JavaScript by unescape(encodeURIComponent(str)),
+ * but this approach may be useful in other languages.
+ *
+ * @param   {string} unicodeString - Unicode string to be encoded as UTF-8.
+ * @returns {string} UTF8-encoded string.
+ */
+export const utf8Encode = function (unicodeString) {
+  if (typeof unicodeString != 'string') throw new TypeError(
+    'parameter ‘unicodeString’ is not a string')
+  const utf8String = unicodeString.replace(
+    /[\u0080-\u07ff]/g,  // U+0080 - U+07FF => 2 bytes 110yyyyy, 10zzzzzz
+    function (c) {
+      var cc = c.charCodeAt(0)
+      return String.fromCharCode(0xc0 | cc >> 6, 0x80 | cc & 0x3f)
+    },
+  ).replace(
+    /[\u0800-\uffff]/g,  // U+0800 - U+FFFF => 3 bytes 1110xxxx, 10yyyyyy, 10zzzzzz
+    function (c) {
+      var cc = c.charCodeAt(0)
+      return String.fromCharCode(0xe0 | cc >> 12, 0x80 | cc >> 6 & 0x3F, 0x80 |
+        cc & 0x3f)
+    },
+  )
+  return utf8String
+}
+
+/**
+ * Decodes utf-8 encoded string back into multi-byte Unicode characters.
+ *
+ * Can be achieved JavaScript by decodeURIComponent(escape(str)),
+ * but this approach may be useful in other languages.
+ *
+ * @param   {string} utf8String - UTF-8 string to be decoded back to Unicode.
+ * @returns {string} Decoded Unicode string.
+ */
+export const utf8Decode = function (utf8String) {
+  if (typeof utf8String != 'string') throw new TypeError(
+    'parameter ‘utf8String’ is not a string')
+  // note: decode 3-byte chars first as decoded 2-byte strings could appear to be 3-byte char!
+  const unicodeString = utf8String.replace(
+    /[\u00e0-\u00ef][\u0080-\u00bf][\u0080-\u00bf]/g,  // 3-byte chars
+    function (c) {  // (note parentheses for precedence)
+      var cc = ((c.charCodeAt(0) & 0x0f) << 12) |
+        ((c.charCodeAt(1) & 0x3f) << 6) | ( c.charCodeAt(2) & 0x3f)
+      return String.fromCharCode(cc)
+    },
+  ).replace(
+    /[\u00c0-\u00df][\u0080-\u00bf]/g,                 // 2-byte chars
+    function (c) {  // (note parentheses for precedence)
+      var cc = (c.charCodeAt(0) & 0x1f) << 6 | c.charCodeAt(1) & 0x3f
+      return String.fromCharCode(cc)
+    },
+  )
+  return unicodeString
+}
 
