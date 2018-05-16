7e5fa03073de34f6dc58993991929a7e8c8bb39b
dinhln89 <lenhatdinh@gmail.com>
2018-05-16 17:57:32 +0700

    Add verify link smart contract to address detail page.

--- client/pages/contracts/verify.vue
+++ client/pages/contracts/verify.vue
@@ -34,25 +34,42 @@
 					<div class="col-sm-4">
 						<div class="form-group">
 							<label for="compiler">Compiler:</label>
-							<select id="compiler" class="form-control" v-model="formCompiler">
+							<select
+								@input="$v.formCompiler.$touch()"
+								:class="($v.formCompiler.$dirty && $v.formCompiler.$invalid) ? 'is-invalid' : ''"
+								id="compiler" class="form-control" v-model="formCompiler">
 								<option v-for="option in compilers" v-bind:value="option">{{ option }}</option>
 							</select>
+							<div class="text-danger" v-if="$v.formCompiler.$dirty && ! $v.formCompiler.required">Compiler is required</div>
 						</div>
 					</div>
 					<div class="col-sm-2">
 						<div class="form-group">
 							<label for="optimization">Optimization:</label>
-							<select id="optimization" class="form-control">
+							<select
+								@input="$v.formOptimization.$touch()"
+								:class="($v.formOptimization.$dirty && $v.formOptimization.$invalid) ? 'is-invalid' : ''"
+								id="optimization" class="form-control">
 								<option value="1">Yes</option>
 								<option value="0">No</option>
 							</select>
+							<div class="text-danger" v-if="$v.formOptimization.$dirty && ! $v.formOptimization.required">Optimization is required</div>
 						</div>
 					</div>
 				</div>
 
 				<div class="form-group">
 					<label for="solidityCode">Enter the Solidity Contract Code below</label>
-					<textarea id="solidityCode" cols="30" rows="10" class="form-control"></textarea>
+					<textarea
+						@input="$v.formCode.$touch()"
+						:class="($v.formCode.$dirty && $v.formCode.$invalid) ? 'is-invalid' : ''"
+						id="solidityCode" cols="30" rows="10" class="form-control"></textarea>
+					<div class="text-danger" v-if="$v.formCode.$dirty && ! $v.formCode.required">Solidity Code is required</div>
+				</div>
+
+				<div class="form-group">
+					<button type="button" class="btn btn-primary mr-1" @click="onSubmitVerifyContract">Submit</button>
+					<button type="button" class="btn btn-secondary" @click="onResetForm">Cancel</button>
 				</div>
 			</div>
 		</div>
@@ -70,6 +87,8 @@
         formContractAddress: '',
         formContractName: '',
         formCompiler: '',
+        formOptimization: '',
+        formCode: '',
       }
     },
     validations: {
@@ -82,9 +101,20 @@
       formCompiler: {
         required,
       },
+      formOptimization: {
+        required,
+      },
+      formCode: {
+        required,
+      },
     },
     mounted () {
       let self = this
+
+      let address = self.$route.query.address
+      if (address) {
+        self.formContractAddress = address
+      }
 
       self.getVersions()
     },
@@ -93,7 +123,34 @@
         let self = this
         let {data} = await self.$axios.get('/api/contracts/soljsons')
 
-	      self.compilers = data
+        self.compilers = data
+      },
+
+      async onSubmitVerifyContract () {
+        let self = this
+        if (self.$v.$error) {
+          return
+        }
+        let body = {
+          contractAddress: self.formContractAddress,
+          contractName: self.formContractName,
+          sourceCode: self.formCode,
+          version: self.formCompiler,
+        }
+
+        let {data} = await self.$axios.post('/api/contracts', body)
+
+        console.log(data)
+      },
+
+      onResetForm () {
+        let self = this
+
+        self.formContractAddress = ''
+        self.formContractName = ''
+        self.formCompiler = ''
+        self.formOptimization = ''
+        self.formCode
       },
     },
   }
