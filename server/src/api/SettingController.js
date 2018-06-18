8c302cb8d21b4c72c3a273894dc9c2d073b4a1ad
dinhln89 <lenhatdinh@gmail.com>
2018-06-18 15:16:48 +0700

    Fixed counter not extractly at home page.

--- server/src/api/SettingController.js
+++ server/src/api/SettingController.js
@@ -10,9 +10,10 @@ SettingController.get('/setting', async (req, res, next) => {
   try {
     // Get total blocks in db.
     let totalBlock = await Block.find().count()
-    let totalAddress = await Account.find().count()
-    let totalToken = await Token.find().count()
-    let totalSmartContract = await Account.find({isContract: true}).count()
+    let totalAddress = await Account.find({status: true}).count()
+    let totalToken = await Token.find({status: true}).count()
+    let totalSmartContract = await Account.find(
+      {status: true, isContract: true}).count()
     let lastBlock = await Block.findOne().sort({number: -1})
 
     return res.json(
