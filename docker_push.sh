c6059dc846adbb4d2da229746fec31fbd2744ec7
etienne-napoleone <etienne@tomochain.com>
2018-07-03 16:34:28 +0700

    Changed tag of docker image

--- docker_push.sh
+++ docker_push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-docker tag tomochain/tomochain-explorer-server tomochain/tomochain-explorer-server:latest
-docker tag tomochain/tomochain-explorer-server tomochain/tomochain-explorer-server:$TRAVIS_BUILD_ID
-docker tag tomochain/tomochain-explorer-client tomochain/tomochain-explorer-client:latest
-docker tag tomochain/tomochain-explorer-client tomochain/tomochain-explorer-client:$TRAVIS_BUILD_ID
-docker push tomochain/tomochain-explorer-server:latest
-docker push tomochain/tomochain-explorer-server:$TRAVIS_BUILD_ID
-docker push tomochain/tomochain-explorer-client:latest
-docker push tomochain/tomochain-explorer-client:$TRAVIS_BUILD_ID
+docker tag tomochain/tomoscan-server tomochain/tomoscan-server:latest
+docker tag tomochain/tomoscan-server tomochain/tomoscan-server:$TRAVIS_BUILD_ID
+docker tag tomochain/tomoscan-client tomochain/tomoscan-client:latest
+docker tag tomochain/tomoscan-client tomochain/tomoscan-client:$TRAVIS_BUILD_ID
+docker push tomochain/tomoscan-server:latest
+docker push tomochain/tomoscan-server:$TRAVIS_BUILD_ID
+docker push tomochain/tomoscan-client:latest
+docker push tomochain/tomoscan-client:$TRAVIS_BUILD_ID
