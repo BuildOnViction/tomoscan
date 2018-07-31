5846e9086c0682589639dde02bdce0414ab68033
etienne-napoleone <etienne@tomochain.com>
2018-07-31 17:48:03 +0700

    Update .travis.yml Fix travis build stages

--- docker_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-docker tag tomochain/tomoscan-server tomochain/tomoscan-server:$1
-docker tag tomochain/tomoscan-client tomochain/tomoscan-client:$1
-docker push tomochain/tomoscan-server:$1
-docker push tomochain/tomoscan-client:$1
