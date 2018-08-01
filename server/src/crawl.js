ccf02f482c8d6cd64c40159a705d076ae8ad7c87
Minh Khai Do <khaihkd@gmail.com>
2018-08-01 15:39:02 +0700

    update crawl to use new model convention

--- server/src/crawl.js
+++ server/src/crawl.js
@@ -1,19 +1,16 @@
 'use strict'
 
 import Web3Util from './helpers/web3'
-import Setting from './models/Setting'
-
 const config = require('config')
-const mongoose = require('mongoose')
 const q = require('./queues')
 
 let sleep = (time) => new Promise((resolve) => setTimeout(resolve, time))
 
 let watch = async () => {
-    // while (true) {
+    while (true) {
         let web3 = await Web3Util.getWeb3()
         let maxBlockNum = await web3.eth.getBlockNumber()
-        let minBlockCrawl = await Setting.findOne({ meta_key: 'min_block_crawl' })
+        let minBlockCrawl = await db.Setting.findOne({ meta_key: 'min_block_crawl' })
         minBlockCrawl = minBlockCrawl ? minBlockCrawl.meta_value : 0
         minBlockCrawl = parseInt(minBlockCrawl)
         if (minBlockCrawl < maxBlockNum) {
@@ -24,44 +21,40 @@ let watch = async () => {
                 await q.create('BlockProcess', {block: i})
                     .priority('normal').removeOnComplete(true).save()
 
-                let setting = await Setting.findOne({ meta_key: 'min_block_crawl' })
+                let setting = await db.Setting.findOne({ meta_key: 'min_block_crawl' })
                 if (!setting) {
-                    setting = new Setting()
-                    setting.meta_key = 'min_block_crawl'
-                    setting.meta_value = 0
+                    setting = await new db.Setting({
+                        meta_key: 'min_block_crawl',
+                        meta_value: 0
+                    })
                 }
                 if (i >= parseInt(setting.meta_value)) {
                     setting.meta_value = i
                 }
                 await setting.save()
+
                 if (i !== 0 && i % 10 === 0){
-                    // console.log('Sleep 30 seconds')
-                    // await sleep(30000)
-                    console.log('process exit')
-                    process.exit(1)
+                    console.log('Sleep 20 seconds')
+                    await sleep(20000)
+                    // console.log('process exit')
+                    // process.exit(1)
                 }
             }
         }
-    // }
+    }
 }
 
-mongoose.connect(config.get('MONGODB_URI'), {useNewUrlParser: true}, (err) => {
-    if (err) {
-        console.log('MongoDB Connection Error. Please make sure that MongoDB is running.')
-        process.exit(1)
-    } else {
-        try {
-            watch()
-        } catch (e) {
-            if (config.get('APP_ENV') === 'prod') {
-                var slack = require('slack-notify')(config.get('SLACK_WEBHOOK_URL'))
-                slack.send({
-                    channel: '#tm_explorer',
-                    text: e
-                })
-            }
-            console.trace(e)
-            console.log(e)
-        }
+
+try {
+    watch()
+} catch (e) {
+    if (config.get('APP_ENV') === 'prod') {
+        let slack = require('slack-notify')(config.get('SLACK_WEBHOOK_URL'))
+        slack.send({
+            channel: '#tm_explorer',
+            text: e
+        })
     }
-})
+    console.trace(e)
+    console.log(e)
+}
