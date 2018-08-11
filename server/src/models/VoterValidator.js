84f32799b48f900b8129078e4dea85ed14452a97
Minh Khai Do <khaihkd@gmail.com>
2018-08-11 11:35:45 +0700

    add missing column

--- server/src/models/VoterValidator.js
+++ server/src/models/VoterValidator.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const VoterValidator = new Schema({
+    voter: { type: String },
     epoch: { type: Number },
     fromBlock: { type: Number },
     toBlock: { type: Number },
