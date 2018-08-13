f652c33efbae0cecfac5ce78b9598fdda6394cb5
Minh Khai Do <khaihkd@gmail.com>
2018-08-13 19:21:08 +0700

    update reward account

--- server/src/queues/rewardAccount.js
+++ server/src/queues/rewardAccount.js
@@ -9,8 +9,18 @@ consumer.processNumber = 6
 consumer.task = async function(job, done) {
     let address = job.data.address
     let balance = job.data.balance
+    let account = await db.Account.findOneAndUpdate({hash: address}, {hash: address}, { upsert: true, new: true })
 
-    await db.Account.findOneAndUpdate({hash: address}, {$inc: {balance: balance}})
+    if (account.balanceNumber) {
+        account.balanceNumber += parseFloat(balance)
+        account.balance = account.balanceNumber.toString()
+
+        await account.save()
+    } else {
+        account.balanceNumber = parseFloat(balance)
+        account.balance = balance.toString()
+        await account.save()
+    }
 
     done()
 
