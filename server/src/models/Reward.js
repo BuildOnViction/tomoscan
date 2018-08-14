dfc821c03ef3493883a32017b4fc287a4bae95a8
Minh Khai Do <khaihkd@gmail.com>
2018-08-14 15:14:59 +0700

    change model reward

--- server/src/models/Reward.js
+++ server/src/models/Reward.js
@@ -5,27 +5,12 @@ const Schema = mongoose.Schema
 
 const Reward = new Schema({
     epoch: { type: Number, index: true },
-    totalReward: { type: Number },
-    fromBlock: { type: Number },
-    toBlock: { type: Number },
-    reward: {
-        masterNode: [
-            {
-                address: { type: String, index: true },
-                numberBlockSigner: { type: Number },
-                lockBalance: { type: Number },
-                reward: { type: Number }
-            }
-        ],
-        voter: [
-            {
-                address: { type: String, index: true },
-                masterNode: { type: String },
-                voteBalance: { type: Number },
-                reward: { type: Number }
-            }
-        ]
-    }
+    startBlock: { type: Number },
+    endBlock: { type: Number },
+    address: {type: String, index: true },
+    isMasterNode: {type: Boolean, index: true },
+    lockBalance: { type: Number },
+    reward: { type: Number }
 }, {
     timestamps: true,
 })
