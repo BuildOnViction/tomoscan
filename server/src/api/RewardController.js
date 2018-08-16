f77df10b98ea55a32e7391c45a725c202023b338
Minh Khai Do <khaihkd@gmail.com>
2018-08-16 11:42:04 +0700

    update api reward

--- server/src/api/RewardController.js
+++ server/src/api/RewardController.js
@@ -1,10 +1,20 @@
 import { Router } from 'express'
-// import { paginate } from '../helpers/utils'
+import { paginate } from '../helpers/utils'
 
 const RewardController = Router()
 
-RewardController.get('/rewards', async (req, res) => {
+RewardController.get('/rewards/:slug', async (req, res) => {
     try {
+        let address = req.params.slug
+        address = address.toLowerCase()
+        let params = {}
+        if (address) {
+            params.query = { address: address }
+        }
+        params.sort = { epoch: -1 }
+        let data = await paginate(req, 'Reward', params)
+
+        return res.json(data)
     } catch (e) {
         console.trace(e)
         console.log(e)
