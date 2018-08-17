290a59db222715fdffa749e0fa471134bb20d49a
Minh Khai Do <khaihkd@gmail.com>
2018-08-17 17:10:05 +0700

    update reward for account

--- server/src/queues/rewardAccount.js
+++ server/src/queues/rewardAccount.js
@@ -1,25 +1,21 @@
 'use strict'
 
 const db = require('../models')
+import AccountHelper from '../helpers/account'
 
 const consumer = {}
 consumer.name = 'AddRewardToAccount'
-consumer.processNumber = 6
+consumer.processNumber = 12
 consumer.task = async function (job, done) {
     let address = job.data.address.toLowerCase()
     let balance = job.data.balance
-    let account = await db.Account.findOneAndUpdate({ hash: address }, { hash: address }, { upsert: true, new: true })
+    let account = await AccountHelper.processAccount(address)
 
-    if (account.balance) {
-        account.balance = (parseFloat(account.balance) + parseFloat(balance)).toString()
-        account.balanceNumber = parseFloat(account.balance)
+    account.balance = (parseFloat(account.balance) + parseFloat(balance)).toString()
+    account.balanceNumber = parseFloat(account.balance)
 
-        await account.save()
-    } else {
-        account.balanceNumber = parseFloat(balance)
-        account.balance = balance.toString()
-        await account.save()
-    }
+    await account.save()
+
 
     done()
 }
