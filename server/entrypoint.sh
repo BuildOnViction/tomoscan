c5cca3aa984c82a26dfcd73aaa71349fe306f6a2
etienne-napoleone <etienne@tomochain.com>
2018-08-18 16:47:30 +0700

    chore(docker): add more docker secrets to entrypoint

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,4 +4,12 @@ if [[ ! -z $SENDGRID_API_KEY_FILE ]]; then
   export SENDGRID_API_KEY=$(cat $SENDGRID_API_KEY_FILE)
 fi
 
+if [[ ! -z $JWT_SECRET_FILE ]]; then
+  export JWT_SECRET=$(cat $JWT_SECRET_FILE)
+fi
+
+if [[ ! -z $APP_SECRET_FILE ]]; then
+  export APP_SECRET=$(cat $APP_SECRET_FILE)
+fi
+
 exec npm "$@"
