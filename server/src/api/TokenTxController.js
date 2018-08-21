fe24ae0c38f613a99621ee0fedb34dd4bd8bce54
Minh Khai Do <khaihkd@gmail.com>
2018-08-21 15:35:22 +0700

    all hash to lower case

--- server/src/api/TokenTxController.js
+++ server/src/api/TokenTxController.js
@@ -6,16 +6,16 @@ const TokenTxController = Router()
 
 TokenTxController.get('/token-txs', async (req, res) => {
     try {
-        let token = req.query.token.toLowerCase()
-        let address = req.query.address.toLowerCase()
+        let token = req.query.token
+        let address = req.query.address
         let params = {}
         params.query = {}
         if (token) {
-            params.query = { address: token }
+            params.query = { address: token.toLowerCase() }
         }
         if (address) {
             params.query = Object.assign(params.query,
-                { $or: { from: address, to: address } })
+                { $or: { from: address.toLowerCase(), to: address.toLowerCase() } })
         }
         params.populate = [{ path: 'block' }]
         let data = await paginate(req, 'TokenTx', params)
