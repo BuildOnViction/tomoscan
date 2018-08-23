c6ad883bca353127d0c0b09119bbc47e0d62e4f5
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-08-23 05:18:29 +0000

    back to count tx in db

--- server/src/helpers/account.js
+++ server/src/helpers/account.js
@@ -49,51 +49,6 @@ let AccountHelper = {
 
         return acc
     },
-    processAccountInQueue:async (hash) => {
-        hash = hash.toLowerCase()
-        let _account = await db.Account.findOne({ hash: hash })
-        _account = _account || {}
-
-        let web3 = await Web3Util.getWeb3()
-
-        let balance = await web3.eth.getBalance(hash)
-        if (_account.balance !== balance) {
-            _account.balance = balance
-            _account.balanceNumber = balance
-        }
-
-        _account.transactionCount = (_account.transactionCount || 0) + 1
-        let txCount = await db.Tx.count({ $or: [ { to: hash }, { from: hash } ] })
-        if (_account.transactionCount !== txCount) {
-            _account.transactionCount = txCount
-        }
-
-        let code = await web3.eth.getCode(hash)
-        if (_account.code !== code) {
-            _account.code = code
-
-            let isToken = await TokenHelper.checkIsToken(code)
-            if (isToken) {
-                // Insert token pending.
-                await db.Token.findOneAndUpdate({ hash: hash },
-                    { hash: hash }, { upsert: true, new: true })
-                const q = require('./index')
-                q.create('TokenProcess', { address: hash })
-                    .priority('normal').removeOnComplete(true).save()
-            }
-            _account.isToken = isToken
-        }
-
-        _account.isContract = (_account.code !== '0x')
-        _account.status = true
-
-        delete _account['_id']
-
-        let acc = await db.Account.findOneAndUpdate({ hash: hash }, _account,
-            { upsert: true, new: true })
-
-        return acc
-    },
     async formatItem (address, baseRank) {
         // Find txn create from.
         let fromTxn = null
