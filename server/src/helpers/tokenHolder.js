63620928009b36ea09f9795b69b989effd88c64f
Minh Khai Do <khaihkd@gmail.com>
2018-08-28 14:50:25 +0700

    refactor queue

--- server/src/helpers/tokenHolder.js
+++ server/src/helpers/tokenHolder.js
@@ -2,14 +2,14 @@
 
 import { convertHexToFloat } from './utils'
 import BigNumber from 'bignumber.js'
+const db = require('../models')
 
 let TokenHolderHelper = {
-    formatHolder (tokenHolder, totalSupply, decimals) {
+    formatHolder: async (tokenHolder, totalSupply, decimals) => {
         if (totalSupply) {
             totalSupply = new BigNumber(totalSupply)
             let quantity = new BigNumber(convertHexToFloat(tokenHolder.quantity, 16))
-            console.log('quantity:', quantity.toString())
-            console.log('totalSupply:', totalSupply.toString())
+
             let percentAge = quantity.div(10 ** decimals).div(totalSupply) * 100
             percentAge = percentAge.toFixed(4)
             percentAge = (percentAge.toString() === '0.0000') ? '0.0001' : percentAge
@@ -17,6 +17,34 @@ let TokenHolderHelper = {
         }
 
         return tokenHolder
+    },
+
+    updateQuality: async (hash, token, quantity) => {
+        let holder = await db.TokenHolder.findOne({ hash: hash, token: token })
+        if (!holder) {
+            // Create new.
+            holder = await db.TokenHolder.create({
+                hash: hash,
+                token: token,
+                quantity: 0
+            })
+        }
+
+        // Convert number to hex.
+        quantity = parseFloat(quantity).toString(16)
+        let quantityCalc = convertHexToFloat(holder.quantity, 16) +
+            convertHexToFloat(quantity, 16)
+        let newQuantity = quantityCalc.toString(16)
+        if (newQuantity.indexOf('-') >= 0) {
+            newQuantity = newQuantity.replace('-', '')
+            newQuantity = newQuantity.padStart(64, '0')
+            newQuantity = '-' + newQuantity
+        } else {
+            newQuantity = newQuantity.padStart(64, '0')
+        }
+        holder.quantity = newQuantity
+        holder.quantityNumber = quantityCalc
+        holder.save()
     }
 }
 
--- server/src/queues/tokenHolder.js
+++ server/src/queues/tokenHolder.js
@@ -1,9 +1,6 @@
 'use strict'
 
-// import BigNumber from 'bignumber.js'
-import { convertHexToFloat } from '../helpers/utils'
-
-const db = require('../models')
+import TokenHolderHelper from '../helpers/tokenHolder'
 
 const consumer = {}
 consumer.name = 'TokenHolderProcess'
@@ -17,52 +14,11 @@ consumer.task = async function (job, done) {
     }
 
     // Add holder from.
-    await updateQuality(token.from, token.address, -token.value)
+    await TokenHolderHelper.updateQuality(token.from, token.address, -token.value)
     // Add holder to.
-    await updateQuality(token.to, token.address, token.value)
+    await TokenHolderHelper.updateQuality(token.to, token.address, token.value)
 
     done()
 }
-
-async function updateQuality (hash, token, quantity) {
-    let holder = await db.TokenHolder.findOne({ hash: hash, token: token })
-    if (!holder) {
-        // Create new.
-        holder = await db.TokenHolder.create({
-            hash: hash,
-            token: token,
-            quantity: 0
-        })
-    }
-
-    // Convert number to hex.
-    quantity = parseFloat(quantity).toString(16)
-    let quantityCalc = convertHexToFloat(holder.quantity, 16) +
-        convertHexToFloat(quantity, 16)
-    let newQuantity = quantityCalc.toString(16)
-    if (newQuantity.indexOf('-') >= 0) {
-        newQuantity = newQuantity.replace('-', '')
-        newQuantity = newQuantity.padStart(64, '0')
-        newQuantity = '-' + newQuantity
-    } else {
-        newQuantity = newQuantity.padStart(64, '0')
-    }
-    holder.quantity = newQuantity
-    holder.quantityNumber = quantityCalc
-    holder.save()
-}
-
-// async function formatItem (tokenHolder, totalSupply) {
-//     if (totalSupply) {
-//         totalSupply = new BigNumber(totalSupply)
-//         let quantity = new BigNumber(convertHexToFloat(tokenHolder.quantity, 16))
-//         let percentAge = quantity.div(totalSupply) * 100
-//         percentAge = percentAge.toFixed(4)
-//         percentAge = (percentAge.toString() === '0.0000') ? '0.0001' : percentAge
-//         tokenHolder.percentAge = percentAge
-//     }
-//
-//     return tokenHolder
-// }
 
 module.exports = consumer
