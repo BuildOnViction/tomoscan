d617c6349b6aa0cf923183a43e9cde07d23cea7f
Minh Khai Do <khaihkd@gmail.com>
2018-08-31 17:40:33 +0700

    remove fn not use

--- server/src/helpers/utils.js
+++ server/src/helpers/utils.js
@@ -101,11 +101,3 @@ export const convertHexToFloat = (str, radix) => {
     }
     return parseInt(parts[0], radix)
 }
-
-export const hex2a = (hexx) => {
-    let hex = hexx.toString();//force conversion
-    let str = '';
-    for (var i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
-}
