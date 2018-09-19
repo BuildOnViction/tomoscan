80f8db266057e09d0e3dc5f0c33503d117e4f076
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-09-19 09:07:31 +0000

    change name to TRC20

--- client/pages/txs/_slug.vue
+++ client/pages/txs/_slug.vue
@@ -146,7 +146,7 @@
                                                                 params: {slug: tokenTx.address}}">
                                                             <span
                                                                 v-if="tokenTx.symbol"
-                                                                v-html="'&nbsp;ERC20 (' + tokenTx.symbol + ')'"/>
+                                                                v-html="'&nbsp;TRC20 (' + tokenTx.symbol + ')'"/>
                                                         </nuxt-link>
                                                         <span>&nbsp;from&nbsp;</span>
                                                         <nuxt-link
