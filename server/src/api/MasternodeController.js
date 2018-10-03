e65f8a04bbf8ee143b899398e408a6fe6c73abff
Vu Pham <pqv199x@hotmail.com>
2018-10-03 11:31:02 +0700

    List of masternode implementation

---
+++ server/src/api/MasternodeController.js
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+import axios from 'axios'
+import urlJoin from 'url-join'
+import config from 'config'
+
+const MasternodeController = Router()
+
+MasternodeController.get('/masternodes', async (req, res) => {
+    try {
+        const tomomasterUrl = config.get('MASTERNODE_URL')
+        const { data } = await axios.get(urlJoin(tomomasterUrl, '/api/candidates'))
+        return res.json(data)
+    } catch (e) {
+        console.trace(e)
+        console.log(e)
+        return res.status(500).send()
+    }
+})
+
+export default MasternodeController
