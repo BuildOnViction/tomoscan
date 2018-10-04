146767b9b2bae6ab9ec7a499b7501926b1524407
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-10-04 08:20:00 +0000

    fix crawler stops after crashing

--- server/src/helpers/errorHandler.js
+++ server/src/helpers/errorHandler.js
@@ -1,9 +1,13 @@
 const EventEmitter = require('events').EventEmitter
 const emitter = new EventEmitter()
+const q = require('../queues')
 
-emitter.on('error', e => {
-    console.error('ERROR!!!', e)
-    process.exit(1)
+emitter.on('error', (e, blockNumber) => {
+    console.error('ERROR!!!', blockNumber, e)
+    console.log('Re-crawl blockNumber', blockNumber)
+    q.create('BlockProcess', { block: blockNumber })
+        .priority('high').removeOnComplete(true).save()
+
 })
 
 module.exports = emitter
