24eac5c55cbef96b88f3d67cbe658bb400778be6
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-10-05 08:53:11 +0000

    miss save file

--- server/src/queues/transaction.js
+++ server/src/queues/transaction.js
@@ -9,12 +9,11 @@ consumer.task = async function (job, done) {
     let hash = job.data.hash.toLowerCase()
     let timestamp = job.data.timestamp
     console.log('Process Transaction: ', hash)
-    await TransactionHelper.crawlTransaction(hash, timestamp, (e) => {
-        if (e) {
-            console.error(consumer.name, e)
-            done(e)
-        }
-    })
+    try {
+        await TransactionHelper.crawlTransaction(hash, timestamp)
+    } catch (e) {
+        done(e)
+    }
 
     done()
 }
