b89d7e29269467a924393a3318698d4ba25a9333
Minh Khai Do <khaihkd@gmail.com>
2018-12-05 15:02:22 +0700

    update count tx without pending

--- server/src/commands/updateSpecialAccount.js
+++ server/src/commands/updateSpecialAccount.js
@@ -24,7 +24,7 @@ const updateSpecialAccount = async () => {
     let map1 = candidates.data.map(async (candidate) => {
         let hash = candidate.candidate.toLowerCase()
         console.info('process candidate', hash)
-        let txCount = await db.Tx.countDocuments({ $or: [{ from: hash }, { to: hash }] })
+        let txCount = await db.Tx.countDocuments({ $or: [{ from: hash }, { to: hash }], isPending: false })
         let minedBlock = await db.Block.countDocuments({ signer: hash })
         let rewardCount = await db.Reward.countDocuments({ address: hash })
         let logCount = await db.Log.countDocuments({ address: hash })
@@ -36,7 +36,7 @@ const updateSpecialAccount = async () => {
         }, { upsert: true })
 
         let owner = candidate.owner.toLowerCase()
-        let txCountOwner = await db.Tx.countDocuments({ $or: [{ from: owner }, { to: owner }] })
+        let txCountOwner = await db.Tx.countDocuments({ $or: [{ from: owner }, { to: owner }], isPending: false })
         let minedBlockOwner = await db.Block.countDocuments({ signer: owner })
         let rewardCountOwner = await db.Reward.countDocuments({ address: owner })
         let logCountOwner = await db.Log.countDocuments({ address: owner })
@@ -54,9 +54,9 @@ const updateSpecialAccount = async () => {
     let map2 = accounts.map(async (acc) => {
         let hash = acc.hash.toLowerCase()
         console.info('process account', hash)
-        let txCount = await db.Tx.countDocuments({ from: hash })
-        txCount += await db.Tx.countDocuments({ to: hash })
-        txCount += await db.Tx.countDocuments({ contractAddress: hash })
+        let txCount = await db.Tx.countDocuments({ from: hash, isPending: false })
+        txCount += await db.Tx.countDocuments({ to: hash, isPending: false })
+        txCount += await db.Tx.countDocuments({ contractAddress: hash, isPending: false })
         let logCount = await db.Log.countDocuments({ address: hash })
         await db.SpecialAccount.updateOne({ hash: hash }, {
             transactionCount: txCount,
