e337202aad3b6a432641efde3f0f04e4f7b39959
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-12-22 03:51:10 +0000

    scale crawler

--- server/src/queues/index.js
+++ server/src/queues/index.js
@@ -20,15 +20,4 @@ const q = kue.createQueue({
 q.setMaxListeners(1000)
 q.watchStuckJobs()
 
-fs.readdirSync(__dirname)
-    .filter(function (file) {
-        return (file.indexOf('.') !== 0) && (file !== 'index.js') && (file.indexOf('.map') < 0)
-    })
-    .forEach(function (file) {
-        let consumer = require(path.join(__dirname, file))
-
-        q.process(consumer.name, consumer.processNumber, consumer.task)
-    })
-
 module.exports = q
-// export default q
