307595aa599f6e39bfec14eb164192b7cc02ec5c
Do Minh Khai <khaihkd@gmail.com>
2019-01-14 11:28:40 +0700

    speed up api account

--- server/src/models/Account.js
+++ server/src/models/Account.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema
 const Account = new Schema({
     hash: { type: String, unique: true, index: true },
     balance: { type: String },
-    balanceNumber: { type: Number, index: true },
+    balanceNumber: Number,
     code: String,
     transactionCount: { type: Number, default: 0 },
     minedBlock: { type: Number, default: 0 },
@@ -25,4 +25,5 @@ const Account = new Schema({
     versionKey: false
 })
 
+Account.index({ balanceNumber: -1 })
 module.exports = mongoose.model('Account', Account)
