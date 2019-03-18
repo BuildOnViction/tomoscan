ce27f2132ce61fcfdad4dd4d669c4f420359722e
Do Minh Khai <khaihkd@gmail.com>
2019-03-18 12:31:35 +0700

    fix wrong token quantity number

--- server/src/helpers/tokenHolder.js
+++ server/src/helpers/tokenHolder.js
@@ -4,6 +4,7 @@ const BigNumber = require('bignumber.js')
 const db = require('../models')
 const TokenHelper = require('./token')
 const Web3Util = require('../helpers/web3')
+const logger = require('../helpers/logger')
 
 let TokenHolderHelper = {
     formatHolder: async (tokenHolder, totalSupply, decimals) => {
@@ -21,31 +22,35 @@ let TokenHolderHelper = {
     },
 
     updateQuality: async (hash, token, quantity) => {
-        let holder = await db.TokenHolder.findOne({ hash: hash, token: token })
-        if (!holder) {
-            // Create new.
-            holder = await db.TokenHolder.create({
-                hash: hash,
-                token: token,
-                quantity: 0
-            })
-        }
-        let tk = await db.Token.findOne({ hash: token })
-        let decimals
-        if (tk) {
-            decimals = tk.decimals
-        } else {
-            let web3 = await Web3Util.getWeb3()
-            let tokenFuncs = await TokenHelper.getTokenFuncs()
-            decimals = await web3.eth.call({ to: token, data: tokenFuncs['decimals'] })
-            decimals = await web3.utils.hexToNumberString(decimals)
-        }
+        try {
+            let holder = await db.TokenHolder.findOne({hash: hash, token: token})
+            if (!holder) {
+                // Create new.
+                holder = await db.TokenHolder.create({
+                    hash: hash,
+                    token: token,
+                    quantity: 0
+                })
+            }
+            let tk = await db.Token.findOne({hash: token})
+            let decimals
+            if (tk) {
+                decimals = tk.decimals
+            } else {
+                let web3 = await Web3Util.getWeb3()
+                let tokenFuncs = await TokenHelper.getTokenFuncs()
+                decimals = await web3.eth.call({to: token, data: tokenFuncs['decimals']})
+                decimals = await web3.utils.hexToNumberString(decimals)
+            }
 
-        let oldQuantity = new BigNumber(holder.quantity || 0)
-        let newQuantity = oldQuantity.plus(quantity)
-        holder.quantity = newQuantity.toString()
-        holder.quantityNumber = newQuantity.dividedBy(10 ** parseInt(decimals))
-        holder.save()
+            let oldQuantity = new BigNumber(holder.quantity || 0)
+            let newQuantity = oldQuantity.plus(quantity)
+            holder.quantity = newQuantity.toString()
+            holder.quantityNumber = newQuantity.dividedBy(10 ** parseInt(decimals)).toNumber()
+            holder.save()
+        } catch (e) {
+            logger.warn('token updateQuality error %s', e)
+        }
     }
 }
 
