2b8b628764ea46c6dd42303f6b42d6c128d0d0a2
Do Minh Khai <khaihkd@gmail.com>
2019-04-10 11:54:10 +0700

    add handle mongo connection error

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -5,13 +5,36 @@ const path = require('path')
 const mongoose = require('mongoose')
 const config = require('config')
 const db = {}
+const logger = require('../helpers/logger')
 
 mongoose.Promise = global.Promise
-mongoose.connect(config.get('MONGODB_URI'), { useCreateIndex: true, useNewUrlParser: true }, (err) => {
-    if (err) {
-        console.log('MongoDB Connection Error. Please make sure that MongoDB is running.')
-        process.exit(1)
-    }
+mongoose.connect(config.get('MONGODB_URI'),
+    { useCreateIndex: true, useNewUrlParser: true, autoReconnect: true }, (err) => {
+        if (err) {
+            console.log('MongoDB Connection Error. Please make sure that MongoDB is running.')
+            process.exit(1)
+        }
+    })
+mongoose.connection.on('connecting', function () {
+    logger.info('connecting to MongoDB...')
+})
+
+mongoose.connection.on('error', function (error) {
+    logger.warn('Error in MongoDb connection: ' + error)
+    mongoose.disconnect()
+})
+mongoose.connection.on('connected', function () {
+    logger.info('MongoDB connected!')
+})
+mongoose.connection.once('open', function () {
+    logger.info('MongoDB connection opened!')
+})
+mongoose.connection.on('reconnected', function () {
+    logger.info('MongoDB reconnected!')
+})
+mongoose.connection.on('disconnected', function () {
+    logger.info('MongoDB disconnected!')
+    mongoose.connect(config.get('MONGODB_URI'), { useCreateIndex: true, useNewUrlParser: true, autoReconnect: true })
 })
 
 // import all file in this dir, except index.js
