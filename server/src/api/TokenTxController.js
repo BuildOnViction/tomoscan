e5868e9706c29ab365965bd9e247246660147ba2
Do Minh Khai <khaihkd@gmail.com>
2019-06-20 17:20:57 +0700

    merge token tx api

--- server/src/api/TokenTxController.js
+++ server/src/api/TokenTxController.js
@@ -7,10 +7,11 @@ const { check, validationResult } = require('express-validator/check')
 
 const TokenTxController = Router()
 
-TokenTxController.get('/token-txs', [
+TokenTxController.get('/token-txs/:tokenType', [
+    check('tokenType').exists().isString().withMessage('trc20/trc21/trc721'),
     check('limit').optional().isInt({ max: 50 }).withMessage('Limit is less than 50 items per page'),
     check('page').optional().isInt().withMessage('Require page is number'),
-    check('address').optional().isLength({ min: 42, max: 42 }).withMessage('Account address is incorrect.'),
+    check('holder').optional().isLength({ min: 42, max: 42 }).withMessage('Account address is incorrect.'),
     check('token').optional().isLength({ min: 42, max: 42 }).withMessage('Token address is incorrect.')
 ], async (req, res) => {
     let errors = validationResult(req)
@@ -18,7 +19,8 @@ TokenTxController.get('/token-txs', [
         return res.status(400).json({ errors: errors.array() })
     }
     let token = req.query.token
-    let address = req.query.address
+    let holder = req.query.holder
+    let tokenType = req.params.tokenType
     try {
         let params = {}
         params.query = {}
@@ -31,58 +33,28 @@ TokenTxController.get('/token-txs', [
                 total = tk.totalTxCount
             }
         }
-        if (address) {
+        if (holder) {
             params.query = Object.assign(params.query,
-                { $or: [{ from: address.toLowerCase() }, { to: address.toLowerCase() }] })
+                { $or: [{ from: holder.toLowerCase() }, { to: holder.toLowerCase() }] })
             total = null
         }
         params.sort = { blockNumber: -1 }
-        let data = await paginate(req, 'TokenTx', params, total)
-
-        let items = data.items
-        if (items.length) {
-            items = await TokenTransactionHelper.formatTokenTransaction(items)
-        }
-        data.items = items
-
-        return res.json(data)
-    } catch (e) {
-        logger.warn('Get list token tx: token %s | address %s error %s', token, address, e)
-        return res.status(500).json({ errors: { message: 'Something error!' } })
-    }
-})
-
-TokenTxController.get('/token-txs/trc21', [
-    check('limit').optional().isInt({ max: 50 }).withMessage('Limit is less than 50 items per page'),
-    check('page').optional().isInt().withMessage('Require page is number'),
-    check('address').optional().isLength({ min: 42, max: 42 }).withMessage('Account address is incorrect.'),
-    check('token').optional().isLength({ min: 42, max: 42 }).withMessage('Token address is incorrect.')
-], async (req, res) => {
-    let errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
-    }
-    let token = req.query.token
-    let address = req.query.address
-    try {
-        let params = {}
-        params.query = {}
-        let total = null
-        if (token) {
-            params.query = { address: token.toLowerCase() }
-
-            let tk = await db.Account.findOne({ hash: token.toLowerCase() })
-            if (tk) {
-                total = tk.totalTxCount
+        let data
+        if (tokenType === 'trc20') {
+            data = await paginate(req, 'TokenTx', params, total)
+        } else if (tokenType === 'trc21') {
+            data = await paginate(req, 'TokenTrc21Tx', params, total)
+        } else if (tokenType === 'trc721') {
+            data = await paginate(req, 'TokenNftTx', params, total)
+        } else {
+            data = {
+                total: total,
+                perPage: parseInt(req.query.limit) || 25,
+                currentPage: parseInt(req.query.page) || 1,
+                pages: 0,
+                items: []
             }
         }
-        if (address) {
-            params.query = Object.assign(params.query,
-                { $or: [{ from: address.toLowerCase() }, { to: address.toLowerCase() }] })
-            total = null
-        }
-        params.sort = { blockNumber: -1 }
-        let data = await paginate(req, 'TokenTrc21Tx', params, total)
 
         let items = data.items
         if (items.length) {
@@ -92,50 +64,7 @@ TokenTxController.get('/token-txs/trc21', [
 
         return res.json(data)
     } catch (e) {
-        logger.warn('Get list token tx: token %s | address %s error %s', token, address, e)
-        return res.status(500).json({ errors: { message: 'Something error!' } })
-    }
-})
-TokenTxController.get('/token-txs/nft', [
-    check('limit').optional().isInt({ max: 50 }).withMessage('Limit is less than 50 items per page'),
-    check('page').optional().isInt().withMessage('Require page is number'),
-    check('address').optional().isLength({ min: 42, max: 42 }).withMessage('Account address is incorrect.'),
-    check('token').optional().isLength({ min: 42, max: 42 }).withMessage('Token address is incorrect.')
-], async (req, res) => {
-    let errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
-    }
-    let token = req.query.token
-    let address = req.query.address
-    try {
-        let params = {}
-        params.query = {}
-        let total = null
-        if (token) {
-            params.query = { address: token.toLowerCase() }
-
-            let tk = await db.Account.findOne({ hash: token.toLowerCase() })
-            if (tk) {
-                total = tk.totalTxCount
-            }
-        }
-        if (address) {
-            params.query = Object.assign(params.query,
-                { $or: [{ from: address.toLowerCase() }, { to: address.toLowerCase() }] })
-        }
-        params.sort = { blockNumber: -1 }
-        let data = await paginate(req, 'TokenNftTx', params, total)
-
-        let items = data.items
-        if (items.length) {
-            items = await TokenTransactionHelper.formatTokenTransaction(items)
-        }
-        data.items = items
-
-        return res.json(data)
-    } catch (e) {
-        logger.warn('Get list token tx: token %s | address %s error %s', token, address, e)
+        logger.warn('Get list token tx: token %s | holder %s error %s', token, holder, e)
         return res.status(500).json({ errors: { message: 'Something error!' } })
     }
 })
