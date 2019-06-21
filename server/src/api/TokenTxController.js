7e0bc634b025e29efe6b4bd5567c842ce41ae6b7
Do Minh Khai <khaihkd@gmail.com>
2019-06-21 10:37:58 +0700

    revert api list trc20 token for wallet & add api list token-txs by tx hash

--- server/src/api/TokenTxController.js
+++ server/src/api/TokenTxController.js
@@ -7,6 +7,51 @@ const { check, validationResult } = require('express-validator/check')
 
 const TokenTxController = Router()
 
+TokenTxController.get('/token-txs', [
+    check('limit').optional().isInt({ max: 50 }).withMessage('Limit is less than 50 items per page'),
+    check('page').optional().isInt().withMessage('Require page is number'),
+    check('address').optional().isLength({ min: 42, max: 42 }).withMessage('Account address is incorrect.'),
+    check('token').optional().isLength({ min: 42, max: 42 }).withMessage('Token address is incorrect.')
+], async (req, res) => {
+    let errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    let token = req.query.token
+    let address = req.query.address
+    try {
+        let params = {}
+        params.query = {}
+        let total = null
+        if (token) {
+            params.query = { address: token.toLowerCase() }
+
+            let tk = await db.Account.findOne({ hash: token.toLowerCase() })
+            if (tk) {
+                total = tk.totalTxCount
+            }
+        }
+        if (address) {
+            params.query = Object.assign(params.query,
+                { $or: [{ from: address.toLowerCase() }, { to: address.toLowerCase() }] })
+            total = null
+        }
+        params.sort = { blockNumber: -1 }
+        let data = await paginate(req, 'TokenTx', params, total)
+
+        let items = data.items
+        if (items.length) {
+            items = await TokenTransactionHelper.formatTokenTransaction(items)
+        }
+        data.items = items
+
+        return res.json(data)
+    } catch (e) {
+        logger.warn('Get list token tx: token %s | address %s error %s', token, address, e)
+        return res.status(500).json({ errors: { message: 'Something error!' } })
+    }
+})
+
 TokenTxController.get('/token-txs/:tokenType', [
     check('tokenType').exists().isString().withMessage('trc20/trc21/trc721'),
     check('limit').optional().isInt({ max: 50 }).withMessage('Limit is less than 50 items per page'),
@@ -65,6 +110,63 @@ TokenTxController.get('/token-txs/:tokenType', [
         return res.json(data)
     } catch (e) {
         logger.warn('Get list token tx: token %s | holder %s error %s', token, holder, e)
+        return res.status(500).json({ errors: { message: 'Something error!' } })
+    }
+})
+
+TokenTxController.get('/token-txs/:tokenType/:txHash', [
+    check('tokenType').exists().isString().withMessage('trc20/trc21/trc721'),
+    check('txHash').exists().isString().withMessage('transaction hash'),
+    check('holder').optional().isLength({ min: 42, max: 42 }).withMessage('Account address is incorrect.'),
+    check('limit').optional().isInt({ max: 50 }).withMessage('Limit is less than 50 items per page'),
+    check('page').optional().isInt().withMessage('Require page is number')
+], async (req, res) => {
+    let errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    let holder = req.query.holder
+    let tokenType = req.params.tokenType
+    let txHash = req.params.txHash
+    let tx = await db.Tx.findOne({ hash: txHash })
+    if (!tx) {
+        return res.status(404).json({ errors: { message: 'Tx hash does not exist!' } })
+    }
+    try {
+        let params = {}
+        params.query = { transactionHash: txHash }
+        if (holder) {
+            params.query = Object.assign(params.query,
+                { $or: [{ from: holder.toLowerCase() }, { to: holder.toLowerCase() }] })
+        }
+        params.sort = { blockNumber: -1 }
+        let data
+        if (tokenType === 'trc20') {
+            data = await paginate(req, 'TokenTx', params, null)
+        } else if (tokenType === 'trc21') {
+            data = await paginate(req, 'TokenTrc21Tx', params, null)
+        } else if (tokenType === 'trc721') {
+            data = await paginate(req, 'TokenNftTx', params, null)
+        } else {
+            data = {
+                total: 0,
+                perPage: parseInt(req.query.limit) || 25,
+                currentPage: parseInt(req.query.page) || 1,
+                pages: 0,
+                items: []
+            }
+        }
+
+        let items = data.items
+        if (items.length) {
+            items = await TokenTransactionHelper.formatTokenTransaction(items)
+        }
+        data.items = items
+        data.transaction = tx
+
+        return res.json(data)
+    } catch (e) {
+        logger.warn('Get list token tx of hash %s | holder %s error %s', txHash, holder, e)
         return res.status(500).json({ errors: { message: 'Something error!' } })
     }
 })
