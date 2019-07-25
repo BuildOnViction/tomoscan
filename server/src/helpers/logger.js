e43319a851fadd0c0533263cd106d82138cb984c
Do Minh Khai <khaihkd@gmail.com>
2019-07-25 09:52:04 +0700

    add timestamp to logger

--- server/src/helpers/logger.js
+++ server/src/helpers/logger.js
@@ -1,15 +1,16 @@
 const { createLogger, format, transports } = require('winston')
-const { combine, printf } = format
+const { combine, printf, timestamp } = format
 const config = require('config')
 
-const lFormat = printf(info => {
-    return `${info.level.toUpperCase()}: ${info.message}`
+const lFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp} ${level.toUpperCase()}: ${message}`
 })
 
 const logger = createLogger({
     level: config.get('logs.level'),
     format: combine(
         format.splat(),
+        timestamp(),
         lFormat
     ),
     transports: [new transports.Console()]
