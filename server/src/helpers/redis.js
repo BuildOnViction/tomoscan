bca10012c7553a21e401fbc6c44b8dccbe8b51ca
Do Minh Khai <khaihkd@gmail.com>
2019-09-26 10:56:58 +0700

    fix missing

--- server/src/helpers/redis.js
+++ server/src/helpers/redis.js
@@ -11,7 +11,7 @@ const client = redis.createClient({
 // default expiry time is 2 hours
 const DEFAULT_EXPIRY_TIME = 2 * 60 * 60
 let RedisHelper = {
-    set: async (name, value) => {
+    set: async (name, value, expiry = null) => {
         // will expiry in 2 hours if not set expiry time
         if (expiry) {
             await client.set(name, value, 'EX', expiry)
