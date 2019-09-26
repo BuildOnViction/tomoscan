601cdff6226916fee86a57605ed9f1ba34ef40ea
Do Minh Khai <khaihkd@gmail.com>
2019-09-26 10:43:14 +0700

    cache usd price to redis

--- server/src/api/SettingController.js
+++ server/src/api/SettingController.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const db = require('../models')
 const Web3Util = require('../helpers/web3')
 const logger = require('../helpers/logger')
-const apiCacheWithRedis = require('../middlewares/apicache')
+const redisHelper = require('../helpers/redis')
 
 const SettingController = Router()
 var tomoUsd = {}
@@ -29,11 +29,20 @@ SettingController.get('/setting', async (req, res) => {
     }
 })
 
-SettingController.get('/setting/usd', apiCacheWithRedis('10 minutes'), async (req, res) => {
+SettingController.get('/setting/usd', async (req, res) => {
     try {
+        let cache = await redisHelper.get('tomo-price')
+        if (cache !== null) {
+            let r = JSON.parse(cache)
+            logger.info('load tomo price from cache')
+            return res.json(r)
+        }
         let url = 'https://api.coingecko.com/api/v3/simple/price?ids=tomochain&vs_currencies=usd'
+
         let { data } = await axios.get(url, { timeout: 5000 })
+
         tomoUsd = data
+        await redisHelper.set('tomo-price', JSON.stringify(data), 10 * 60)
     } catch (e) {
         logger.warn(e)
     }
