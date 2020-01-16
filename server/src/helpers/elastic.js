7b4a69185f7909513cbd2025a6c64383926187cf
Do Minh Khai <khaihkd@gmail.com>
2020-01-16 14:19:37 +0700

    add elastic search

---
+++ server/src/helpers/elastic.js
@@ -0,0 +1,42 @@
+const config = require('config')
+
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({ node: config.get('ELASTICSEARCH') })
+
+let ElasticHelper = {
+    index: async (id, index, body) => {
+        await client.index({
+            id: id,
+            index: index,
+            opType: 'index',
+            body: body
+        })
+    },
+    search: async (index, query, sort, limit, page) => {
+        if (Object.keys(query).length > 0) {
+            let { body } = await client.search({
+                index: index,
+                body: {
+                    query: query,
+                    sort: sort,
+                    size: limit,
+                    from: (page - 1) * limit
+                }
+            })
+            // })
+            return body
+        } else {
+            let { body } = await client.search({
+                index: index,
+                body: {
+                    sort: sort,
+                    size: limit,
+                    from: (page - 1) * limit
+                }
+            })
+            return body
+        }
+    }
+}
+
+module.exports = ElasticHelper
